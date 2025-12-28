c1c169d9dfed4075aa17c1ed19a3a085d5647b39
Mostafa Hosseini <kavrindev@gmail.com>
2025-12-28 19:58:47 +0330

    Initial commit

---
+++ userservice/src/main/java/com/fitness/userservice/model/User.java
@@ -0,0 +1,35 @@
+package com.fitness.userservice.model;
+
+import jakarta.persistence.*;
+import lombok.Data;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "users")
+@Data // Lombok annotation to generate getters, setters, toString, etc.
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private String id;
+
+    @Column(unique = true, nullable = false)
+    private String email;
+
+    @Column(nullable = false)
+    private String password;
+    private String firstName;
+    private String lastName;
+
+    @Enumerated(EnumType.STRING) // Store enum as String in the database
+    private UserRole role = UserRole.USER;
+
+    @CreationTimestamp // Automatically set the creation timestamp
+    private LocalDateTime createdAt;
+
+    @UpdateTimestamp
+    private LocalDateTime updatedAt;
+}
